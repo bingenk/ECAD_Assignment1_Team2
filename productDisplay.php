fc4166c52c22b8926f8730800fbeadfcda76965b
bingenk <s10244655@connect.np.edu.sg>
2024-01-30 12:28:08 +0800

    product listing on search done

--- productDisplay.php
+++ productDisplay.php
@@ -12,7 +12,8 @@ if (isset($_GET['category']) && is_array($_GET['category'])) {
     $query = "SELECT p.ProductID, p.ProductTitle, p.ProductDesc, p.ProductImage, p.Price, p.Offered, p.OfferedPrice, p.OfferStartDate, p.OfferEndDate FROM product p 
               INNER JOIN catproduct cp ON p.ProductID = cp.ProductID 
               INNER JOIN category c ON cp.CategoryID = c.CategoryID 
-              WHERE c.CatName IN ($placeholders)";
+              WHERE c.CatName IN ($placeholders)
+              ORDER BY p.ProductTitle";
     
     $stmt = $conn->prepare($query);
     // Dynamic parameter binding
@@ -60,16 +61,7 @@ if (isset($_GET['category']) && is_array($_GET['category'])) {
             echo "</p>";
             echo "<a href='#' class='read-more-link'>Read more</a>";
             echo "<a href='#' class='read-less-link'>Read less</a>";
-            echo "</div>"; // .description-container
-
-            // Ratings
-            echo "<div class='showcase-rating'>";
-            echo "<ion-icon name='star'></ion-icon>";
-            echo "<ion-icon name='star'></ion-icon>";
-            echo "<ion-icon name='star'></ion-icon>";
-            echo "<ion-icon name='star-outline'></ion-icon>";
-            echo "<ion-icon name='star-outline'></ion-icon>";
-            echo "</div>"; // .showcase-rating
+            echo "</div>"; // .description-container            
 
             // Price box
             echo "<div class='price-box'>";
@@ -97,7 +89,7 @@ include('footer.php');
 
 <style>
 .product-grid {
-    display: flex;
+    display: flex-end;
     flex-wrap: wrap;
     gap: 1rem;
     justify-content: center;
@@ -135,8 +127,9 @@ include('footer.php');
 }
 
 .read-more-link,
-.read-less-link {
-    position: absolute;
+.read-less-link {   
+    color: black;
+    font-size: smaller;
     bottom: 0;
     right: 0;
 }
--- search.php
+++ search.php
@@ -1,38 +1,33 @@
 <?php 
-//Session included in header.php
 include("header.php"); // Include the Page Layout header
 ?>
 
-
-
 <?php
-// The non-empty search keyword is sent to server
 if (isset($_GET["search"]) && trim($_GET['search']) != "") {
-    // To Do (DIY): Retrieve list of product records with "ProductTitle" or "ProductDesc"
     include_once("mysql_conn.php");
     $qry = "SELECT * FROM product WHERE ProductTitle LIKE '%" . $_GET["search"] . "%' OR ProductDesc LIKE '%" . $_GET["search"] . "%'";
-    $result=$conn->query($qry);
-	// contains the keyword entered by shopper, and display them in a table.
-	echo "<div class='row' style='padding:5px'>";// Start a new row
+    $result = $conn->query($qry);
 
-	// Left column - display a text link showing the product's name ,
-	//               display the selling price in red in a new paragraph
-    if($result-> num_rows>0){
-    while ($row=$result-> fetch_array()){
-        echo "<div class='row' style='padding:5px'>";// Start a new row
-        $product ="productDetails.php?pid=$row[ProductID]";
-        echo "<div class='col-8'>" ;// 67% of row width
-        echo "<p><a href=$product>$row[ProductTitle]</a></p>";
-        echo"</div>";
-        echo"</div>"; //End of a row
-    }//End of a row
-	// To Do (DIY): End of Code
-}
-else{
-    echo "No record found!";
-}
+    if ($result->num_rows > 0) {
+        echo '<div class="shopping-cart">';       
+
+        while ($row = $result->fetch_array()) {
+            echo '<div class="product">';
+            echo '<div class="product-image">';
+            echo "<img src='Images/Products/$row[ProductImage]' />";
+            echo '</div>';
+            echo '<div class="product-details">';
+            echo "<div class='product-title'><a href='productDetails.php?pid=$row[ProductID]' style=color:black>$row[ProductTitle]</a></div>";
+            echo "<p class='product-description'>$row[ProductDesc]</p>";
+            echo '</div>';
+            echo '</div>';
+        }
+
+        echo '</div>';
+    } else {
+        echo "<p>No products found for the search query.</p>";
+    }
 }
 
-echo "</div>"; // End of container
 include("footer.php"); // Include the Page Layout footer
 ?>
